dbc09158549682f1090c7973bde05638392f4749
wildcat2k21 <djerse12@mail.ru>
2025-04-10 12:18:19 +0300

    Добавлена поддержка комбинированных коротких флагов

--- lab_1_max/src/main.cpp
+++ lab_1_max/src/main.cpp
@@ -15,17 +15,40 @@ int main(int argc, char** argv) {
     // Обработка флагов
     for (int i = 1; i < argc; ++i) {
         std::string arg = argv[i];
-        if (arg == "--lines" || arg == "-l") {
+
+        // Комбинированные короткие флаги, например -lwc
+        if (arg[0] == '-' && arg.size() > 1 && arg[1] != '-') {
+            for (size_t j = 1; j < arg.size(); ++j) {
+                switch (arg[j]) {
+                    case 'l': count_lines = true; break;
+                    case 'w': count_words = true; break;
+                    case 'c': count_bytes = true; break;
+                    case 'm': count_chars = true; break;
+                    default:
+                        std::cerr << "Unknown flag: -" << arg[j] << "\n";
+                        return 1;
+                }
+            }
+        }
+        // Длинные флаги
+        else if (arg == "--lines") {
             count_lines = true;
-        } else if (arg == "--words" || arg == "-w") {
+        } else if (arg == "--words") {
             count_words = true;
-        } else if (arg == "--bytes" || arg == "-c") {
+        } else if (arg == "--bytes") {
             count_bytes = true;
-        } else if (arg == "--chars" || arg == "-m") {
+        } else if (arg == "--chars") {
             count_chars = true;
-        } else {
+        }
+        // Имена файлов
+        else {
             filenames.push_back(arg);
         }
+    }
+
+    if (filenames.empty()) {
+        std::cerr << "Error: No input files provided.\n";
+        return 1;
     }
 
     // Обработка файлов
